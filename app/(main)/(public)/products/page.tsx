82d4238a6a9df0e47e414c1845e8dcaa43214768
uabj8000 <uabj8000@gmail.com>
2023-09-07 17:30:31 -0300

    stage 06

--- app/(main)/(public)/products/page.tsx
+++ app/(main)/(public)/products/page.tsx
@@ -19,28 +19,13 @@ import './locals.css';
 
 export default function Shoes() {
 
-  let hash = useRef('null');
-
-  const galleryContainer = useRef<any>({});
-  const thumbsContainer = useRef<any>({});
+  const [hash, setHash] = useState('null');
+  const [galleryContainer, setGalleryContainer] = useState<any>([]);
+  const [thumbsContainer, setThumbsContainer] = useState<any>([]);
 
   useEffect(() => {
-    if (location.hash) hash.current = location.hash.substring(1);
+    if (location.hash) setHash(location.hash.substring(1));
   }, []);
-
-  const getGallerySwiper = useCallback(
-    (id: string) => {
-      return galleryContainer.current[id];
-    },
-    [galleryContainer]
-  );
-
-  const getThumbsSwiper = useCallback(
-    (id: string) => {
-      return thumbsContainer.current[id];
-    },
-    [thumbsContainer]
-  );
 
   const commonOptions: SwiperOptions = {
     modules: [Mousewheel, Keyboard, Pagination, Navigation, HashNavigation, FreeMode, Thumbs, Controller],
@@ -66,15 +51,31 @@ export default function Shoes() {
     watchSlidesProgress: true,
   }
 
-  const galleryOptions: (product: any) => SwiperOptions = (product: any) => ({
+  const getGallerySwiper = useCallback(
+    (i: number) => {
+      return galleryContainer[i];
+    },
+    [galleryContainer]
+  );
+
+  const getThumbsSwiper = useCallback(
+    (i: number) => {
+      return thumbsContainer[i];
+    },
+    [thumbsContainer]
+  );
+
+  const galleryOptions: (i: number) => SwiperOptions = (i: number) => ({
     ...commonOptions,
     pagination: false,
     watchSlidesProgress: true,
     nested: true,
-    controller: { control: getThumbsSwiper(product.id) },
+    thumbs: {
+      swiper: getThumbsSwiper(i)
+    }
   });
 
-  const thumbsOptions: (product: any) => SwiperOptions = (product: any) => ({
+  const thumbsOptions: (i: number) => SwiperOptions = (i: number) => ({
     ...commonOptions,
     direction: 'vertical',
     spaceBetween: 10,
@@ -84,63 +85,49 @@ export default function Shoes() {
     keyboard: false,
     freeMode: true,
     watchSlidesProgress: true,
-    controller: { control: getGallerySwiper(product.id), by: "container" },
   });
 
-
   useEffect(() => {
+    for (const key in galleryContainer) {
+      const gallerySwiper = galleryContainer[key];
+      const thumbsSwiper = thumbsContainer[key];
 
-    console.log(galleryContainer.current)
-
-    console.log(galleryContainer.current)
-
-    for (const key in galleryContainer.current) {
-      const gallerySwiper = galleryContainer.current[key];
       gallerySwiper.enable();
-    }
 
-    for (const key in galleryContainer.current) {
-      const gallerySwiper = galleryContainer.current[key];
-      gallerySwiper.enable();
-      gallerySwiper.update({
-        thumbs: { swiper: thumbsContainer.current[key] },
-      });
+      thumbsSwiper.enable();
     }
-
   }, [thumbsContainer, galleryContainer]);
 
   const onSwiper = (swiper: any) => {
-
   };
 
   const gOnSwiper = (swiper: any) => {
-    galleryContainer.current[swiper.id] = swiper;
+    setGalleryContainer((gallery: any) => [...gallery, swiper]);
+    swiper.disable();
   };
 
   const tOnSwiper = (swiper: any) => {
-    thumbsContainer.current[swiper.id] = swiper;
+    setThumbsContainer((thumbs: any) => [...thumbs, swiper]);
+    swiper.disable();
   };
 
   const onSlideChange = (swiper: any) => {
-
   };
 
   const gOnSlideChange = (swiper: any) => {
-
   };
 
   const tOnSlideChange = (swiper: any) => {
-
   };
 
-  const { data, isLoading, error } = useAllProductsShoes(2, hash.current);
+  const { data, isLoading, error } = useAllProductsShoes(2, hash);
 
   if (isLoading) return <Loading />;
   if (error) console.log(error);
 
   let products = data.flat();
 
-  if (hash.current === 'null') products.shift();
+  if (hash === 'null') products.shift();
 
   return (
     <div className="h-screen flex justify-center items-center">
@@ -148,14 +135,11 @@ export default function Shoes() {
         {products.map((e, i) => {
           return (
             <SwiperSlide className="flex flex-col justify-center items-center" key={i} data-hash={e?.slug}>
-
               <div className="w-11/12 flex flex-col lg:flex-row-reverse">
-
                 <div className="w-full h-96 gap-4 flex lg:w-7/12">
                   <div className="relative w-10/12">
                     <div className="w-full h-full">
-                      <Swiper className={`gallery gallery-${i} bg-white rounded-md`} {...galleryOptions(e)} onSlideChange={gOnSlideChange} onSwiper={gOnSwiper}>
-
+                      <Swiper className={`gallery gallery-${i} bg-white rounded-md`} {...galleryOptions(i)} onSlideChange={gOnSlideChange} onSwiper={gOnSwiper}>
                         {e?.images.map((e, i) => {
                           return (
                             <SwiperSlide className="bg-white p-6 rounded-md flex flex-row justify-center items-center" key={i}>
@@ -163,15 +147,12 @@ export default function Shoes() {
                             </SwiperSlide>
                           )
                         })}
-
                       </Swiper>
                     </div>
-
                     <Link href="#" className="more-button absolute z-10 -bottom-12 -right-12 m-4 inline-block rounded-md bg-slate-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus:bg-indigo-600">Ver</Link>
                   </div>
                   <div className="w-2/12 h-3/6">
-                    <Swiper className={`thumbs thumbs-${i}`} {...thumbsOptions(e)} onSlideChange={tOnSlideChange} onSwiper={tOnSwiper}>
-
+                    <Swiper className={`thumbs thumbs-${i}`} {...thumbsOptions(i)} onSlideChange={tOnSlideChange} onSwiper={tOnSwiper}>
                       {e?.images.map((e, i) => {
                         return (
                           <SwiperSlide className="bg-white rounded-md opacity-50 flex flex-col justify-center items-center" key={i}>
@@ -179,19 +160,15 @@ export default function Shoes() {
                           </SwiperSlide>
                         )
                       })}
-
                     </Swiper>
                   </div>
                 </div>
 
                 <div className="info w-10/12 lg:w-5/12">
-
                   <h1 className="m-4 md:text-2xl lg:text-2xl">{e?.name}</h1>
-
                   <h2 className="m-4 md:text-2xl lg:text-2xl">{e?.price}</h2>
 
                   <Link href="#" className="more-button m-4 inline-block rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus:bg-indigo-600">Veja Mais</Link>
-
                 </div>
 
               </div>
