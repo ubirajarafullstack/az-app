500a01903ae401b40e79347ad073c909825415c7
uabj8000 <uabj8000@gmail.com>
2023-09-15 07:40:59 -0300

    working in progress

--- app/(main)/(public)/products/detail/[slug]/page.tsx
+++ app/(main)/(public)/products/detail/[slug]/page.tsx
@@ -86,58 +86,58 @@ export default function Shoes({ params }: { params: { slug: string } }) {
       {product?.map((e, i) => {
         return (
           <>
-          <div className="w-11/12 p-4 text-xs">{e.department} / {e.spirit} / {e.productCategory}</div>
-          <div className="w-11/12 flex flex-col lg:flex-row-reverse" key={i}>
+            <div className="w-11/12 p-4 text-xs">{e.department} / {e.spirit} / {e.productCategory}</div>
+            <div className="w-11/12 flex flex-col lg:flex-row-reverse" key={i}>
 
-            <div className="w-full h-96 sm:h-[484px] gap-4 flex lg:w-7/12">
-              <div className="w-10/12">
-                
-                <div className="w-full h-full">
-                  <Swiper className="gallery bg-white rounded-md" {...galleryOptions}>
+              <div className="w-full h-96 sm:h-[484px] gap-4 flex lg:w-7/12">
+                <div className="w-10/12">
+
+                  <div className="w-full h-full">
+                    <Swiper className="gallery bg-white rounded-md" {...galleryOptions}>
+
+                      {e?.images.map((e, i) => {
+                        return (
+                          <SwiperSlide className="bg-white p-6 rounded-md flex flex-row justify-center items-center" key={i}>
+                            <img className="block object-contain" src={e.url} alt="" />
+                          </SwiperSlide>
+                        )
+                      })}
+
+                    </Swiper>
+                  </div>
+
+                </div>
+
+                <div className="w-2/12 h-3/6">
+                  <Swiper className="thumbs" {...thumbsOptions} onSwiper={setThumbsContainer}>
 
                     {e?.images.map((e, i) => {
                       return (
-                        <SwiperSlide className="bg-white p-6 rounded-md flex flex-row justify-center items-center" key={i}>
-                          <img className="block object-contain" src={e.url} alt="" />
+                        <SwiperSlide className="bg-white rounded-md opacity-50 flex flex-col justify-center items-center" key={i}>
+                          <img className="w-full h-full block object-contain" src={e.url} alt="" />
                         </SwiperSlide>
                       )
                     })}
 
                   </Swiper>
                 </div>
-
               </div>
 
-              <div className="w-2/12 h-3/6">
-                <Swiper className="thumbs" {...thumbsOptions} onSwiper={setThumbsContainer}>
+              <div className="info w-full lg:w-5/12">
 
-                  {e?.images.map((e, i) => {
-                    return (
-                      <SwiperSlide className="bg-white rounded-md opacity-50 flex flex-col justify-center items-center" key={i}>
-                        <img className="w-full h-full block object-contain" src={e.url} alt="" />
-                      </SwiperSlide>
-                    )
-                  })}
+                <div className="flex">
+                  <div className="w-10/12 flex flex-col justify-between items-start sm:flex-row sm:items-center lg:items-start">
+                    <h1 className="m-4 py-2">{e?.name}</h1>
 
-                </Swiper>
-              </div>
-            </div>
+                    <h2 className="m-4 mt-0 sm:mt-4 sm:py-2">{e?.price}</h2>
+                  </div>
 
-            <div className="info w-full lg:w-5/12">
-
-              <div className="flex">
-                <div className="w-10/12 flex flex-col justify-between items-start sm:flex-row sm:items-center lg:items-start">
-                  <h1 className="m-4 py-2">{e?.name}</h1>
-
-                  <h2 className="m-4 mt-0 sm:mt-4 sm:py-2">{e?.price}</h2>
+                  <div className="w-2/12">
+                    <Link href="#" className="more-button m-4 inline-block rounded-md bg-slate-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus:bg-indigo-600">Go</Link>
+                  </div>
                 </div>
 
-                <div className="w-2/12">
-                  <Link href="#" className="more-button m-4 inline-block rounded-md bg-slate-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus:bg-indigo-600">Go</Link>
-                </div>
-              </div>
-
-              {/* <div>
+                {/* <div>
                 {e?.productType.colors.map((e, i) => {
                   return (
                     <span key={i} className="block h-4 w-4" style={{ background: e.hex }}></span>
@@ -145,13 +145,13 @@ export default function Shoes({ params }: { params: { slug: string } }) {
                 })}
               </div> */}
 
-              <div className="w-10/12 m-4" dangerouslySetInnerHTML={{ __html: e!.description.html }} />
-              
-              <Link href="#" className="more-button m-4 inline-block rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus:bg-indigo-600">{e?.buttonLabel}</Link>
-              <div className="m-4" dangerouslySetInnerHTML={{ __html: e!.highlights.html }} />
-              
+                <div className="w-10/12 m-4" dangerouslySetInnerHTML={{ __html: e!.description.html }} />
+
+                <Link href="#" className="more-button m-4 inline-block rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus:bg-indigo-600">{e?.buttonLabel}</Link>
+                <div className="m-4" dangerouslySetInnerHTML={{ __html: e!.highlights.html }} />
+
+              </div>
             </div>
-          </div>
           </>
         )
       })}
--- app/(main)/(public)/products/page.tsx
+++ app/(main)/(public)/products/page.tsx
@@ -172,10 +172,10 @@ export default function Products() {
                         //console.log('index', index)
                         return (
                           <SwiperSlide className="flex flex-col justify-center items-center" key={index}>
-                            
+
                             <div className="w-11/12 p-4 text-xs">{edge.node.department} / {edge.node.spirit} / {edge.node.productCategory}</div>
                             <div className="w-11/12 flex flex-col lg:flex-row-reverse">
-                              
+
                               <div className="w-full h-96 sm:h-[484px] gap-4 flex lg:w-7/12">
 
                                 <div className="relative w-10/12">
@@ -193,7 +193,7 @@ export default function Products() {
                                     </Swiper>
                                   </div>
 
-                                  <Link href="#" className="more-button absolute z-10 -bottom-12 -right-12 m-4 inline-block px-3 py-2 rounded-md bg-slate-400 hover:bg-indigo-600 focus:bg-indigo-600 text-sm font-semibold text-white shadow-sm">Ver</Link>
+                                  <Link href="#" className="more-button absolute z-10 -bottom-12 -right-12 m-4 inline-block px-3 py-2 rounded-md bg-white hover:bg-white focus:bg-white text-sm font-semibold text-black drop-shadow-2xl">Ver</Link>
                                 </div>
 
                                 <div className="w-2/12 h-3/6">
@@ -214,7 +214,7 @@ export default function Products() {
                                 <h1 className="m-4 md:text-2xl lg:text-2xl">{edge.node.name}</h1>
                                 <h2 className="m-4 md:text-2xl lg:text-2xl">{edge.node.price}</h2>
 
-                                <Link href="#" className="more-button m-4 inline-block px-3 py-2 rounded-md bg-slate-400 hover:bg-indigo-600 focus:bg-indigo-600 text-sm font-semibold text-white shadow-sm">Veja Mais</Link>
+                                <Link href="#" className="more-button m-4 inline-block px-3 py-2 rounded-md bg-white hover:bg-white focus:bg-white text-sm font-semibold text-black drop-shadow-2xl">Veja Mais</Link>
                               </div>
 
                             </div>
@@ -231,9 +231,9 @@ export default function Products() {
           </Swiper>
         )}
 
-        <div className="absolute z-20 bottom-0 left-0 p-4 flex flex-col w-full">
+        <div className="absolute z-20 bottom-0 left-0 p-6 flex flex-col w-full">
           <button
-            className="loading-more-button self-center justify-self-center flex items-center justify-center px-3 py-2 rounded-md bg-white hover:bg-white focus:bg-white font-semibold text-black text-sm"
+            className="loading-more-button self-center justify-self-center flex items-center justify-center px-3 py-2 rounded-md bg-white hover:bg-white focus:bg-white font-semibold text-black text-sm drop-shadow-2xl"
             onClick={() => {
               fetchNextPage();
               //setMainContainer([])
