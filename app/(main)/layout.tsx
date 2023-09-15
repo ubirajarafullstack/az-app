9b9a94fd00597fc9c3f1767ff11d5f050ab72fa6
uabj8000 <uabj8000@gmail.com>
2023-09-15 09:50:52 -0300

    working in progress

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,9 +4,9 @@ import './locals.css';
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <nav className="fixed top-0 z-50 backdrop-blur-sm bg-black/90 w-full p-4 flex justify-center items-center">
+      <nav className="fixed top-0 z-50 backdrop-blur-sm bg-white/90 w-full p-4 flex justify-center items-center">
         <div className="w-11/12">
-          <h1><Link href="/" className="text-white">az-app</Link></h1>
+          <h1><Link href="/" className="text-black">az-app</Link></h1>
         </div>
       </nav>
       <main>
