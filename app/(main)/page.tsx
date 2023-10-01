429f7f604df4aeade4ae0f1e2eb8bd115abe0fdd
uabj8000 <uabj8000@gmail.com>
2023-10-01 19:22:30 -0300

    cleaning, deleting

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex gap-2 justify-center min-h-screen items-center">
       {"{ Home }"}
-      <Link href={{ pathname: "/customer", /* query: { urlparam: "value" }, */ }}>Customer</Link>
+      
       <Link href={{ pathname: "/products", /* query: { urlparam: "value" }, */ }}>Products</Link>
     </div>
   )
