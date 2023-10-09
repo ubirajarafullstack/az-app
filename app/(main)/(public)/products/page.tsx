0590ea7eba3b203f484cb1393ac096be57c1663c
uabj8000 <uabj8000@gmail.com>
2023-10-09 10:38:42 -0300

    products, loading-more-button, loading-more-button position

--- app/(main)/(public)/products/page.tsx
+++ app/(main)/(public)/products/page.tsx
@@ -411,8 +411,8 @@ export default function Products() {
               z-40 
               bottom-0 
               right-1/2 
-              translate-x-10 
-              lg:right-0 
+              translate-x-1/2 
+              lg:right-4 
               lg:translate-x-0 
               flex 
               items-center 
@@ -439,7 +439,7 @@ export default function Products() {
             )
             : (data?.pages.length ?? 0) < totalProducts
               ? <span dangerouslySetInnerHTML={{ __html: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"></path></svg>' }}></span>
-              : <span dangerouslySetInnerHTML={{ __html: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M0 8a8 8 0 1 0 16 0A8 8 0 0 0 0 8zm5.904 2.803a.5.5 0 1 1-.707-.707L9.293 6H6.525a.5.5 0 1 1 0-1H10.5a.5.5 0 0 1 .5.5v3.975a.5.5 0 0 1-1 0V6.707l-4.096 4.096z"></path></svg>' }}></span>}
+              : <span dangerouslySetInnerHTML={{ __html: '' }}></span>}
         </button>
 
 
