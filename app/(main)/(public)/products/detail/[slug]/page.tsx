ede091e3c76b7a75d5cbd800b373b5592aea48e7
uabj8000 <uabj8000@gmail.com>
2023-10-09 09:28:01 -0300

    detail-page, added rating value - star

--- app/(main)/(public)/products/detail/[slug]/page.tsx
+++ app/(main)/(public)/products/detail/[slug]/page.tsx
@@ -248,6 +248,7 @@ export default function Detail({ params }: { params: { slug: string } }) {
                         <p>Categoria: {item.productType.bookCategory}</p>
                         {/* @ts-ignore */}
                         <p>Dimensões: {item.productType.dimension}</p>
+                        <p>Nota : {item.rating} <svg className="inline-block text-lg" stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 20 20" aria-hidden="true" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></p>
                       </div>
                     </>
                   ) : (
@@ -276,7 +277,7 @@ export default function Detail({ params }: { params: { slug: string } }) {
 
                         {/* @ts-ignore */}
                         <p>Categoria: {item.productType.shoeCategory}</p>
-
+                        <p>Nota : {item.rating} <svg className="inline-block text-lg" stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 20 20" aria-hidden="true" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></p>
                       </div>
                     </>
                   )}
