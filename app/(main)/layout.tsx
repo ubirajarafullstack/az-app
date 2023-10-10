f1751d48a7e1b3bd1e029d0193312c8750d5911f
uabj8000 <uabj8000@gmail.com>
2023-10-10 08:57:32 -0300

    review nav bar size, height, padding

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,7 +4,7 @@ import './locals.css';
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <nav className="fixed top-0 z-30 w-full p-4 flex justify-center items-center backdrop-blur-sm bg-black/90 dark:bg-black/90 text-white dark:text-white">
+      <nav className="fixed top-0 z-30 w-full p-2 flex justify-center items-center backdrop-blur-sm bg-black/90 dark:bg-black/90 text-white dark:text-white">
         <div className="w-11/12">
           <h1><Link href="/">az-app</Link></h1>
         </div>
