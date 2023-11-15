0732e88fdca9ab3cc6fbeade423d572a2a6b50df
uabj8000 <uabj8000@gmail.com>
2023-11-15 10:51:33 -0300

    home page, tuning link to products

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,10 +2,15 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="flex gap-2 justify-center min-h-screen items-center">
+    <div className="flex gap-4 justify-center min-h-screen items-center">
       {"{ Home }"}
       
-      <Link href={{ pathname: "/products", /* query: { urlparam: "value" }, */ }}>Products</Link>
+      <Link 
+        className="decoration-2 underline-offset-4 hover:underline focus:underline" 
+        href={{ pathname: "/products", /* query: { urlparam: "value" }, */ }}
+      >
+        Produtos
+      </Link>
     </div>
   )
 }
